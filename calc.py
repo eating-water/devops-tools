57f5a1db3621d412868d853ddc266ae672f0fd68
eating-water <nithinseenivasan75@gmail.com>
2025-04-23 11:37:55 +0530

    added app.py

--- calc.py
+++
@@ -1,9 +0,0 @@
-"""This module provides calculator functions like add and subtract."""
-
-def add(a, b):
-    """Return the sum of a and b."""
-    return a + b
-
-def subtract(a, b):
-    """Return the difference of a and b."""
-    return a - b
